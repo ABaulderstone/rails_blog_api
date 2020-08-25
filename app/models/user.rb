546329e3cf97e978a95793de874e1192f5288a08
Abaulderstone <alex.baulderstone@gmail.com>
2020-08-25 17:23:40 +0930

    added category model and associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
     validates :email, presence: true,uniqueness: true, format: { with: VALID_EMAIL_REGEX }
     enum role: {user: 0, admin: 1}
+    has_many :posts
+
 end
